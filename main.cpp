01458c36b09ea14929ecd74dbc8a85c0408a865e
liangcansun <liangcan.sun@poolin.com>
2021-06-17 16:22:12 +0800

    90+++ 123

--- main.cpp
+++ main.cpp
@@ -8,7 +8,7 @@ void test(){
     this_thread::sleep_for(chrono::milliseconds(10));
 }
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    std::cout << "Hello, Wssorld!" << std::endl;
     thread t1(test);
     t1.join();
 
