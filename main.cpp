fa2df68e187d6a45d8cfcff33b8893422ca4460f
liangcansun <liangcan.sun@poolin.com>
2021-07-05 10:03:09 +0800

    7565

--- main.cpp
+++ main.cpp
@@ -8,7 +8,7 @@ void test(){
     this_thread::sleep_for(chrono::milliseconds(10));
 }
 int main() {
-    std::cout << "Hello, Wssorld211321234323123sds!" << std::endl;
+    std::cout << "Hello, Wssorld2113212说的是34323123sds!" << std::endl;
     thread t1(test);
     t1.join();
 
